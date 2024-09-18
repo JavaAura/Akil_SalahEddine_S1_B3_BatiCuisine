241be120f23476c4214404161d00de1194e20e4c
salah <akilsalaheddine203@gmail.com>
2024-09-18 15:52:53 +0200

    creation des entities

--- src/main/java/org/aura/models/Materiel.java
+++ src/main/java/org/aura/models/Materiel.java
@@ -1,4 +1,48 @@
 package org.aura.models;
 
-public class Materiel {
+public class Materiel extends Composants {
+    private double coutUnitaire;
+    private double quantite;
+    private  double coutTransport;
+    private  double coefficientQualite;
+
+    public Materiel(int id, String nom, String typeComposant, double tauxTVA, double coutUnitaire, double quantite, double coutTransport, double coefficientQualite) {
+        super(id, nom, typeComposant, tauxTVA);
+        this.coutUnitaire = coutUnitaire;
+        this.quantite = quantite;
+        this.coutTransport = coutTransport;
+        this.coefficientQualite = coefficientQualite;
+    }
+
+    public double getCoutUnitaire() {
+        return coutUnitaire;
+    }
+
+    public void setCoutUnitaire(double coutUnitaire) {
+        this.coutUnitaire = coutUnitaire;
+    }
+
+    public double getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(double quantite) {
+        this.quantite = quantite;
+    }
+
+    public double getCoutTransport() {
+        return coutTransport;
+    }
+
+    public void setCoutTransport(double coutTransport) {
+        this.coutTransport = coutTransport;
+    }
+
+    public double getCoefficientQualite() {
+        return coefficientQualite;
+    }
+
+    public void setCoefficientQualite(double coefficientQualite) {
+        this.coefficientQualite = coefficientQualite;
+    }
 }
--- src/main/java/org/aura/models/Projet.java
+++ src/main/java/org/aura/models/Projet.java
@@ -1,4 +1,57 @@
 package org.aura.models;
 
 public class Projet {
+    private int id;
+    private String nomProjet;
+    private double margeBeneficiaire;
+    private double coutTotal;
+    private etat etatProjet;
+
+    public Projet(int id, String nomProjet, double margeBeneficiaire, double coutTotal, etat etatProjet) {
+        this.id = id;
+        this.nomProjet = nomProjet;
+        this.margeBeneficiaire = margeBeneficiaire;
+        this.coutTotal = coutTotal;
+        this.etatProjet = etatProjet;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomProjet() {
+        return nomProjet;
+    }
+
+    public void setNomProjet(String nomProjet) {
+        this.nomProjet = nomProjet;
+    }
+
+    public double getMargeBeneficiaire() {
+        return margeBeneficiaire;
+    }
+
+    public void setMargeBeneficiaire(double margeBeneficiaire) {
+        this.margeBeneficiaire = margeBeneficiaire;
+    }
+
+    public double getCoutTotal() {
+        return coutTotal;
+    }
+
+    public void setCoutTotal(double coutTotal) {
+        this.coutTotal = coutTotal;
+    }
+
+    public etat getEtatProjet() {
+        return etatProjet;
+    }
+
+    public void setEtatProjet(etat etatProjet) {
+        this.etatProjet = etatProjet;
+    }
 }
