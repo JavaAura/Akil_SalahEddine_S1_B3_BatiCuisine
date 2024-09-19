fa518f8104c3f61be825afabcfc9da7f83af35e4
salah <akilsalaheddine203@gmail.com>
2024-09-19 16:32:20 +0200

    Add Main d'oeuvre services

--- src/main/java/org/aura/services/implementation/workForceImplServ.java
+++ src/main/java/org/aura/services/implementation/workForceImplServ.java
@@ -1,4 +1,36 @@
 package org.aura.services.implementation;
 
-public class workForceImplServ {
+import org.aura.models.workforce;
+import org.aura.repository.implementation.mainDoeuvreRepoImpl;
+import org.aura.services.interfaces.workForceInterfaceServ;
+import org.aura.utils.LoggerUtils;
+
+
+import java.util.Optional;
+
+public class workForceImplServ implements workForceInterfaceServ {
+
+    private final mainDoeuvreRepoImpl mainDoeuvreRepo;
+
+    public workForceImplServ() {
+        this.mainDoeuvreRepo = new mainDoeuvreRepoImpl() ;
+    }
+
+    @Override
+    public workforce getWorkForce(int id) {
+        return Optional.ofNullable(mainDoeuvreRepo.findWorkForceById(id))
+                .orElseGet(()-> {
+                    LoggerUtils.logInfo("Main d'oeuvre no trouvé");
+                    return null;
+                });
+    }
+
+    @Override
+    public void createWorkForce(workforce workforce) {
+        if (workforce!=null){
+            mainDoeuvreRepo.addWorkForce(workforce);
+        }else {
+            LoggerUtils.logInfo("Mai d'oeuvre n'a pas créer");
+        }
+    }
 }
