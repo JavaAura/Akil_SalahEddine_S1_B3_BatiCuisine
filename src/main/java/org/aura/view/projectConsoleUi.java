7dff4f698e1ef350b35ef3158a9cd697e5d24495
salah <akilsalaheddine203@gmail.com>
2024-09-22 03:26:34 +0200

    add mainDoeuvre

--- src/main/java/org/aura/view/projectConsoleUi.java
+++ src/main/java/org/aura/view/projectConsoleUi.java
@@ -4,6 +4,8 @@ import org.aura.models.Client;
 import org.aura.models.Projet;
 import org.aura.models.enums.Etat;
 import org.aura.services.implementation.projetImplServ;
+import org.aura.utils.LoggerUtils;
+
 import static org.aura.utils.LoggerUtils.logInfo;
 
 import java.util.Scanner;
@@ -52,17 +54,31 @@ public class projectConsoleUi {
             logInfo("Erreur lors de la création du projet.");
             return;
         }
-        materialConsoleUi.ajouterMateriel(scanner, projetId);
-        workForceConsoleUi.ajouterMainOeuvre(scanner, projetId);
-    }
-    public static void calculateProjectCost(Scanner scanner) {
-        System.out.println("--- Calcul du coût d'un projet ---");
-
-        System.out.println("Calcul du coût en cours...");
+         materialConsoleUi.ajouterMateriel(scanner, projetId);
+         workForceConsoleUi.ajouterMainOeuvre(scanner, projetId);
     }
 
-    public static void main(String[] args) {
-        projectConsoleUi projectConsoleUi  = new projectConsoleUi();
-        projectConsoleUi.creeProjet(new Scanner(System.in));
+    public static void calculateProjectCout(Scanner scanner , materialConsoleUi materialConsoleUi ,workForceConsoleUi workForceConsoleUi,int projetId) {
+        LoggerUtils.logInfo("--- Calcul du coût d'un projet ---");
+        double coutTotalMateriaux = materialConsoleUi.ajouterMateriel(scanner, projetId);
+        double coutTotalMainOeuvre = workForceConsoleUi.ajouterMainOeuvre(scanner, projetId);
+        double coutTotalAvantMarge = coutTotalMateriaux + coutTotalMainOeuvre;
+        LoggerUtils.logInfo("Souhaitez-vous appliquer une marge bénéficiaire au projet ? (y/n) : ");
+        String appliqueMarge = scanner.next();
+        double margeBeneficiaire = 0.0;
+        if (appliqueMarge.equalsIgnoreCase("y")) {
+            LoggerUtils.logInfo("Entrez le pourcentage de marge bénéficiaire (%) : ");
+            double pourcentageMarge = scanner.nextDouble();
+            margeBeneficiaire = coutTotalAvantMarge * (pourcentageMarge / 100);
+            double coutTotalFinal = coutTotalAvantMarge + margeBeneficiaire;
+            LoggerUtils.logInfo("Calcul du coût en cours...");
+            System.out.println("--- Résultat du Calcul ---");
+            System.out.println("Coût total avant marge : " + coutTotalAvantMarge + " DH");
+            if (margeBeneficiaire > 0) {
+                System.out.println("Marge bénéficiaire (" + (margeBeneficiaire / coutTotalAvantMarge * 100) + "%) : " + margeBeneficiaire + " DH");
+            }
+            System.out.println("Coût total final du projet : " + coutTotalFinal + " DH");
+        }
+
     }
 }
