ac5890e8121f153336f0b0106e2102cba93cdf72
salah <akilsalaheddine203@gmail.com>
2024-09-24 22:44:02 +0200

    Readme Update

--- src/main/java/org/aura/repository/interfaces/clientRepoInterface.java
+++ src/main/java/org/aura/repository/interfaces/clientRepoInterface.java
@@ -2,10 +2,7 @@ package org.aura.repository.interfaces;
 
 import org.aura.models.Client;
 
-import java.util.List;
-
 public interface clientRepoInterface {
-    List<Client> findAllClients();
     Client findClientById(int id);
     Client findClientByName(String name);
     int addClient(Client client);
